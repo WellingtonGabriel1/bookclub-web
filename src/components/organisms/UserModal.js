4d90d9471fa4bfd503fd43f6ca60142d8b066438
WellingtonGabriel1 <WellingtonGabriel7700@gmail.com>
2023-12-06 07:58:48 -0300

    feat: image upload integration

--- src/components/organisms/UserModal.js
+++ src/components/organisms/UserModal.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import {
   Flex,
   Drawer,
@@ -7,17 +8,79 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
-  Avatar
+  Avatar,
+  useToast,
+  Icon
 } from '@chakra-ui/react'
 import { Text, Button } from 'components/atoms'
 import { Input } from 'components/molecules'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { useMutation } from 'react-query'
+import { updateUserCall, updateUserAvatar } from 'services/api/requests'
+import { setUser } from 'services/store/slices/user'
+import { MdModeEditOutline } from 'react-icons/md'
 
 export const UserModal = ({ onClose }) => {
+  const inputFileRef = useRef()
+  const toast = useToast()
   const userStore = useSelector((state) => state.user)
-  const { values, handleChange, errors } = useFormik({
+  const dispatch = useDispatch()
+
+  const mutation = useMutation((data) => updateUserCall(data), {
+    onError: (error) => {
+      toast({
+        title: 'Falha ao atualizar usuário.',
+        description:
+          error?.response?.data?.error || 'Por favor, tente novamente.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      })
+    },
+    onSuccess: (data) => {
+      toast({
+        title: 'Usuário atualizado com sucesso.',
+        status: 'success',
+        duration: 6000,
+        isClosable: true
+      })
+      dispatch(
+        setUser({
+          user: data?.data?.user
+        })
+      )
+    }
+  })
+
+  const mutationAvatar = useMutation((data) => updateUserAvatar(data), {
+    onError: (error) => {
+      toast({
+        title: 'Falha ao atualizar avatar do usuário.',
+        description:
+          error?.response?.data?.error || 'Por favor, tente novamente.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      })
+    },
+    onSuccess: (data) => {
+      toast({
+        title: 'Avatar do usuário atualizado com sucesso.',
+        status: 'success',
+        duration: 6000,
+        isClosable: true
+      })
+      dispatch(
+        setUser({
+          user: data?.data
+        })
+      )
+    }
+  })
+
+  const { handleSubmit, values, handleChange, errors } = useFormik({
     initialValues: {
       name: userStore?.user?.name,
       email: userStore?.user?.email
@@ -30,8 +93,25 @@ export const UserModal = ({ onClose }) => {
         .email('E-mail inválido')
         .required('E-mail é obrigatório.')
     }),
-    onSubmit: (data) => {}
+    onSubmit: (data) => {
+      mutation.mutate(data)
+    }
   })
+
+  const onChangeImage = async (event) => {
+    const file = event?.target?.files[0]
+    const type = file?.type
+
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      const base64 = reader.result
+      mutationAvatar.mutate({
+        mime: type,
+        base64
+      })
+    }
+  }
 
   return (
     <Drawer size="sm" isOpen={true} placement="right" onClose={onClose}>
@@ -44,16 +124,39 @@ export const UserModal = ({ onClose }) => {
 
         <DrawerBody>
           <Flex alignItems="center" justifyContent="center" w="100%">
+            <input
+              ref={inputFileRef}
+              style={{ display: 'none' }}
+              onChange={onChangeImage}
+              type="file"
+              accept="image/*"
+            />
             <Avatar
+              cursor="pointer"
               name={userStore?.user?.name}
               src={userStore?.user?.avatar_url}
-              w={['42px', '100px']}
-              h={['42px', '100px']}
+              w="100px"
+              h="100px"
               borderWidth="4px"
               borderColor="brand.primary"
               bg="brand.greyLight"
               color="brand.black"
+              onClick={() => inputFileRef?.current?.click()}
             />
+            <Flex
+              margin="-22px"
+              cursor="pointer"
+              position="relative"
+              w="32px"
+              h="32px"
+              bg="brand.primary"
+              borderRadius="16px"
+              top="36px"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Icon boxSize="18px" color="brand.Dark" as={MdModeEditOutline} />
+            </Flex>
           </Flex>
           <Input
             type="text"
@@ -74,7 +177,12 @@ export const UserModal = ({ onClose }) => {
             mt="16px"
             placeholder="E-mail"
           />
-          <Button w="100%" mt="64px">
+          <Button
+            onClick={handleSubmit}
+            isLoading={mutation.isLoading || mutationAvatar.isLoading}
+            w="100%"
+            mt="64px"
+          >
             Atualizar
           </Button>
         </DrawerBody>
